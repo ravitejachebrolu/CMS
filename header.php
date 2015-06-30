0f92c946a3123b7007a862ece126063e27496429
ravitejachebrolu <rchebrol@hawk.iit.edu>
2015-06-30 00:38:45 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>Spykeps </title>
+	<link href="stylesheets/public.css" media = 
+	         "all" rel = "stylesheet" type= "text/css" />
+</head>
+<body>
+   <div id = "header">
+     <h1> Spykeps </h1>
+   </div>
